d503497c82fe0f5e3a8da754885567a3310498df
Shantanu Prakash <shantanuprakash@Shantanus-MacBook-Air.local>
2021-04-03 20:09:36 +0530

    adding min balance as config

--- myWallet/myWallet/settings.py
+++ myWallet/myWallet/settings.py
@@ -82,6 +82,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'OPTIONS': {
+            'timeout': 5,
+        }
     }
 }
 
@@ -130,3 +133,5 @@ CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:8081',
 )
+
+MIN_BALANCE = 0.0
